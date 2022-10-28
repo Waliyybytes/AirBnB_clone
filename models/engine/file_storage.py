45885e750cba53f72d62cdda41b00fe443bc7f27
Ripeplantain <irvingmanny@gmail.com>
2022-10-28 13:52:25 +0000

    will continue storage in the evening

---
+++ models/engine/file_storage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Help stores dictionary to a JSON format for serialization and deserialization
+"""
+
+class FileStorage:
+    """
+    File Storage class that serializes and deserializes the instance
+    """
+
+    def __init__(self):
+        pass
